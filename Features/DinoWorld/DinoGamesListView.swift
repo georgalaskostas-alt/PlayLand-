6e05b6eb5b56e8234a61377a811ded6d1698e1ca
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-08-26 01:20:42 +0300

    Replace missing Dino World icons with installed dinosaur artwork

--- Features/DinoWorld/DinoGamesListView.swift
+++ Features/DinoWorld/DinoGamesListView.swift
@@ -6,10 +6,10 @@ struct DinoGamesListView: View {
 
     private var games: [MenuItem] {
         [
-            MenuItem(id: "dino_dig", title: Loc.t("dino.dig.title"), description: Loc.t("dino.dig.desc"), iconImageName: AppAssets.PlannedGameIcons.dinoDig),
-            MenuItem(id: "dino_match", title: Loc.t("dino.match.title"), description: Loc.t("dino.match.desc"), iconImageName: AppAssets.PlannedGameIcons.dinoMatch),
-            MenuItem(id: "dino_farm", title: Loc.t("dino.farm.title"), description: Loc.t("dino.farm.desc"), iconImageName: AppAssets.PlannedGameIcons.dinoFarm),
-            MenuItem(id: "dino_sort", title: Loc.t("dino.sort.title"), description: Loc.t("dino.sort.desc"), iconImageName: AppAssets.PlannedGameIcons.dinoSort)
+            MenuItem(id: "dino_dig", title: Loc.t("dino.dig.title"), description: Loc.t("dino.dig.desc"), iconImageName: "triceratops_rpg_neutral"),
+            MenuItem(id: "dino_match", title: Loc.t("dino.match.title"), description: Loc.t("dino.match.desc"), iconImageName: "stegosaurus_rpg_neutral"),
+            MenuItem(id: "dino_farm", title: Loc.t("dino.farm.title"), description: Loc.t("dino.farm.desc"), iconImageName: "brachiosaurus_rpg_neutral"),
+            MenuItem(id: "dino_sort", title: Loc.t("dino.sort.title"), description: Loc.t("dino.sort.desc"), iconImageName: "ankylosaurus_rpg_neutral")
         ]
     }
 
