1ea2d8cbed1f0ad842a58262fed938fa3180ebb0
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-08-26 01:40:32 +0300

    Wire orientation handling into PlayLand app lifecycle

--- App/PlayLandApp.swift
+++ App/PlayLandApp.swift
@@ -2,6 +2,7 @@ import SwiftUI
 
 @main
 struct PlayLandApp: App {
+    @UIApplicationDelegateAdaptor(PlayLandAppDelegate.self) private var appDelegate
     @StateObject private var progressManager = ProgressViewModel()
     @StateObject private var appSettings = AppSettings.shared
 
