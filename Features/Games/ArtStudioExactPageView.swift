81e1b37f49fa476cb95c15054f8cb6c8aa50fb2b
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-09-03 22:39:24 +0300

    Center Art Studio guide inside canvas

--- Features/Games/ArtStudioExactPageView.swift
+++ Features/Games/ArtStudioExactPageView.swift
@@ -4,18 +4,28 @@ struct ArtStudioExactPageView: View {
     let index: Int
 
     var body: some View {
-        Group {
-            if index < 30 {
-                Image(uiImage: ArtStudioDirectPages.image(at: index))
-                    .resizable()
-                    .renderingMode(.original)
-                    .interpolation(.high)
-                    .scaledToFit()
-            } else {
-                ArtStudioProceduralPageView(index: index - 30)
+        GeometryReader { proxy in
+            let availableWidth = max(proxy.size.width - 24, 0)
+            let availableHeight = max(proxy.size.height - 24, 0)
+            let sourceAspect: CGFloat = 15.0 / 11.0
+
+            let fittedWidth = min(availableWidth, availableHeight * sourceAspect)
+            let fittedHeight = fittedWidth / sourceAspect
+
+            Group {
+                if index < 30 {
+                    Image(uiImage: ArtStudioDirectPages.image(at: index))
+                        .resizable()
+                        .renderingMode(.original)
+                        .interpolation(.high)
+                        .scaledToFit()
+                } else {
+                    ArtStudioProceduralPageView(index: index - 30)
+                }
             }
+            .frame(width: fittedWidth, height: fittedHeight, alignment: .center)
+            .position(x: proxy.size.width / 2, y: proxy.size.height / 2)
         }
-        .aspectRatio(15.0 / 11.0, contentMode: .fit)
         .background(Color.white)
         .preferredColorScheme(.light)
     }
