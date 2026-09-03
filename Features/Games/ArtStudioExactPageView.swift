3dabe6f6c94200153e6ed6e44a084d404148a8db
georgalaskostas-alt <georgalas.kostas@gmail.com>
2026-09-03 22:30:55 +0300

    Render 200 procedural Art Studio drawings

--- Features/Games/ArtStudioExactPageView.swift
+++ Features/Games/ArtStudioExactPageView.swift
@@ -4,13 +4,19 @@ struct ArtStudioExactPageView: View {
     let index: Int
 
     var body: some View {
-        Image(uiImage: ArtStudioDirectPages.image(at: index))
-            .resizable()
-            .renderingMode(.original)
-            .interpolation(.high)
-            .scaledToFit()
-            .aspectRatio(15.0 / 11.0, contentMode: .fit)
-            .background(Color.white)
-            .preferredColorScheme(.light)
+        Group {
+            if index < 30 {
+                Image(uiImage: ArtStudioDirectPages.image(at: index))
+                    .resizable()
+                    .renderingMode(.original)
+                    .interpolation(.high)
+                    .scaledToFit()
+            } else {
+                ArtStudioProceduralPageView(index: index - 30)
+            }
+        }
+        .aspectRatio(15.0 / 11.0, contentMode: .fit)
+        .background(Color.white)
+        .preferredColorScheme(.light)
     }
 }
